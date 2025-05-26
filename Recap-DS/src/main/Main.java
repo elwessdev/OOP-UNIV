64cb96fcd752c70e1f4f35db8023bae67eea64ae
elwessdev <benaliosama3@gmail.com>
2025-05-26 11:08:32 +0100

    RecapDS

--- Recap-DS/src/main/Main.java
+++ Recap-DS/src/main/Main.java
@@ -3,10 +3,10 @@ package main;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JButton;
@@ -33,10 +33,6 @@ public class Main extends JFrame {
 		setTitle("Recap DS");
 		setLayout(new GridLayout(10,2));
 		
-//		add(new JLabel("SBN: "));
-//		isbn = new JTextField();
-//		add(isbn);
-		
 		add(new JLabel("Titre: "));
 		titre = new JTextField();
 		add(titre);
@@ -50,7 +46,10 @@ public class Main extends JFrame {
 		add(annee);
 		
 		zone = new JTextArea();
-		add(zone);
+		zone.setLineWrap(true);
+		zone.setWrapStyleWord(true);
+		zone.setEditable(false);
+		
 		
 		ajouter = new JButton("Ajouter");
 		rechercher = new JButton("Rechercher");
@@ -61,38 +60,157 @@ public class Main extends JFrame {
 		add(supprimer);
 		add(afficher);
 		
-		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
+		add(zone);
+		
 		
 		ajouter.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-//				String isbnValue = isbn.getText();
 				String TitreValue = titre.getText();
 				String AuteurValue = auteur.getText();
 				String AnneeValue = annee.getText();
 				if(TitreValue.isEmpty()||AuteurValue.isEmpty()||AnneeValue.isEmpty()) {
 					JOptionPane.showMessageDialog(null,"All fields are required","Error",JOptionPane.ERROR_MESSAGE);
+					return;
 				}
 				if(!AnneeValue.matches(".*\\d.*")||AnneeValue.length()!=4) {
 					JOptionPane.showMessageDialog(null,"Annee is not valid","Error",JOptionPane.ERROR_MESSAGE);
+					return;
 				}
-				try(
-					PreparedStatement ps = conn.prepareStatement("INSERT INTO livres (titre,auteur,annee) VALUES (?,?)");
+				
+				// Check Book
+				try (
+					ResultSet checkBook = exeSelect("SELECT 1 FROM livres WHERE titre = ?", TitreValue);
 				){
-					ps.setString(1,TitreValue);
-	        		ps.setString(2,AuteurValue);
-	        		ps.setString(3,AnneeValue);
-	        		ps.executeUpdate();
-	        		System.out.println("livre added");
+					if (checkBook.next()) {
+						JOptionPane.showMessageDialog(null,"Book with this title already exist","Error",JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				} catch(SQLException eCheckBook) {
+					eCheckBook.printStackTrace();
+				}
+				
+				// Add Book
+				try{
+					int addBook = exeUpdate("INSERT INTO livres (titre,auteur,annee) VALUES (?,?,?)",TitreValue,AuteurValue,Integer.parseInt(AnneeValue));
+					if(addBook==0) {
+						JOptionPane.showMessageDialog(null,"Something wrong when add book","Error",JOptionPane.ERROR_MESSAGE);
+					}
+	        		titre.setText(" ");
+	        		auteur.setText(" ");
+	        		annee.setText(" ");
+	        		JOptionPane.showMessageDialog(null, "Book Added", "Success", JOptionPane.INFORMATION_MESSAGE);
 				} catch(SQLException e1) {
 		            e1.printStackTrace();
 				}
 			}
 		});
+		
+		rechercher.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String titreRecherche = JOptionPane.showInputDialog(
+	            		null, 
+	            		"Titre chercher", 
+	            		"Titre", 
+	            		JOptionPane.PLAIN_MESSAGE
+	            );
+				if (titreRecherche.length()==0) {
+					return;
+				}
+				try(
+					ResultSet cherBook = exeSelect("SELECT * FROM livres WHERE titre = ?", titreRecherche);
+				) {
+					if(cherBook.next()) {
+						zone.setText(
+								"Title: "+ cherBook.getString("titre") + "\n" +
+								"Auteur: " + cherBook.getString("auteur") + "\n" +
+								"Annee: " + cherBook.getInt("annee")
+						);
+						return;
+					}
+					zone.setText("No book with this name");
+				} catch(SQLException e1) {
+		            e1.printStackTrace();
+				}
+			}
+		});
+		
+		supprimer.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String titireDel = JOptionPane.showInputDialog(
+	            		null, 
+	            		"Titre for delete", 
+	            		"Titre", 
+	            		JOptionPane.PLAIN_MESSAGE
+	            );
+				if (titireDel.length()==0) {
+					return;
+				}
+				try{
+					int delBook = exeUpdate("DELETE FROM livres WHERE titre = ?", titireDel);
+					if(delBook==0) {
+						JOptionPane.showMessageDialog(null, "No book with this name", "Error", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+					JOptionPane.showMessageDialog(null, "Book Deleted", "Success", JOptionPane.INFORMATION_MESSAGE);
+				} catch(SQLException e1) {
+		            System.out.print(e1);
+				}
+			}
+		});
+		
+		
+		afficher.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try(
+					ResultSet allBooks = exeSelect("SELECT * FROM livres");
+				) {
+					boolean hasData = false;
+					String all = "";
+					while (allBooks.next()) {
+		                hasData = true;
+		                all += "Title: "+ allBooks.getString("titre") + "\n" +
+								"Auteur: " + allBooks.getString("auteur") + "\n" +
+								"Annee: " + allBooks.getInt("annee") + "\n-------\n";
+		            }
+
+		            if (!hasData) {
+		                zone.setText("No books");
+		            } else {
+		                zone.setText(all);
+		            }
+				} catch(SQLException e1) {
+					System.out.print(e1);
+				}
+			}
+		});
+
+		
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 	
+	
+	public int exeUpdate(String sql, Object... params) throws SQLException {
+		try(PreparedStatement stmt = conn.prepareStatement(sql)){
+			for (int i = 0; i < params.length; i++) {
+	            stmt.setObject(i+1, params[i]);
+	        }
+	        return stmt.executeUpdate();
+		}
+	}
+	
+	public ResultSet exeSelect(String sql, Object... params) throws SQLException {
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		for(int i = 0; i < params.length; i++) {
+			stmt.setObject(i+1, params[i]);
+		}
+		return stmt.executeQuery();
+	}
 	
 	public static void main(String[] args) {
 		new Main();
